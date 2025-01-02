c896ef5d92bd4d522964f484c881be886c2f1f2d
MrJorgx <jhidaf00@estudiantes.unileon.es>
2025-01-02 13:27:19 +0100

    Creado el login.fxml e inicio del LoginController

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,5 +1,20 @@
 package controllers;
 
 public class LoginController {
-    
+
+    public boolean login(String username, String password) {
+        // verificar credenciales del usuario
+        // true si las credenciales son correctas, sino false
+        return authenticate(username, password);
+    }
+
+    public void logout() {
+        // cerrar sesión
+    }
+
+    private boolean authenticate(String username, String password) {
+        // autenticación (verificar base de datos)
+        // Este es un método privado auxiliar
+        return "user".equals(username) && "pass".equals(password);
+    }
 }
